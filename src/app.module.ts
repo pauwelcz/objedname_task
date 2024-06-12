2682ac81042b0657f3b6d0d366c13b9771f7f396
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-12 21:07:46 +0200

    add: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
-
+import { AuthModule } from './auth/auth.module';
+import { Customer } from './customers/entities/customer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_SQL_HOST || 'localhost',
+      port: parseInt(process.env.MYSQL_SQL_PORT) || 3306,
+      username: process.env.MYSQL_ROOT_PASSWORD || 'root',
+      password: process.env.MYSQL_ROOT_PASSWORD || 'root',
+      database: process.env.MYSQL_DATABASE || 'objedname',
+      entities: [Customer],
+      synchronize: false,
+    }),
+    OrdersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
